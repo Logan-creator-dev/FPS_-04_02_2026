411d848b072b8c84f0f819fb3a01684e69a93708
Logan Villaret <l.villaret@ensi.lan>
2026-02-06 16:55:02 +0100

    FPS_04_02_2026

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 
 public class LaserController : MonoBehaviour
 {
-
+    [SerializeField] private AudioClip _audioClip;
     [SerializeField] private Transform _originTransform;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
@@ -12,6 +19,9 @@ public class LaserController : MonoBehaviour
         
         if (Input.GetButtonDown("Fire2"))
         {
+            
+            _audioSource.PlayOneShot(_audioClip);
+            
             Ray ray = new Ray(_originTransform.position, _originTransform.forward);
 
             if (Physics.Raycast(ray, out RaycastHit hit))
